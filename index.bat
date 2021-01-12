8c116586292757e6fdee952529337d1487ffbcb4
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:24:21 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
 @echo off
-echo NUL>_.class&&del /s /f /q *.class
 cls
-javac com/krzem/rsa/Main.java&&java com/krzem/rsa/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/rsa/Main.java&&jar cvmf ../manifest.mf ../build/rsa.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="rsa.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/rsa.jar
+:end
