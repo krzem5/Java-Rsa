cf4b2845ad067b87fd06f812e57ceb4ecbe8a8b9
Krzem <krzem5.dev@gmail.com>
2021-04-10 10:53:37 +0200

    Push Update 04/10/2021, 10:53:20

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/rsa/Main.java&&jar cvmf ../manifest.mf ../build/rsa.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="rsa.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/rsa.jar
-:end
+python build.py %*
